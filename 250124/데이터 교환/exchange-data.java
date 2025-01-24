bf86d3301e1dd16afe95a25f4850fc673a696b4b
TalkingPotato90 <138276030+TalkingPotato90@users.noreply.github.com>
2025-01-24 12:12:46 +0900

    Title: 데이터 교환, Time: 86ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 250124/데이터 교환/exchange-data.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        int a = 5;
+        int b = 6;
+        int c = 7;
+        int temp1 = b;
+        int temp2 = c;
+
+        b = a;
+        c = temp1;
+        a = temp2;
+
+        System.out.printf("%d\n%d\n%d",a,b,c);
+
+    }
+}
