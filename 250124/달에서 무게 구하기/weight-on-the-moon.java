2810a125dbeb4554261cadc73607ec322c166ee4
TalkingPotato90 <138276030+TalkingPotato90@users.noreply.github.com>
2025-01-24 11:37:39 +0900

    Title: 달에서 무게 구하기, Time: 94ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 250124/달에서 무게 구하기/weight-on-the-moon.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        int weight = 13;
+        float gravity = 0.165f;
+
+        System.out.printf("%d * %.6f = %.6f", weight, gravity, weight*gravity);
+    }
+}
