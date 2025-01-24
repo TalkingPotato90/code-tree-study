656c62259757391511d23873b5890179a69fbfaa
TalkingPotato90 <138276030+TalkingPotato90@users.noreply.github.com>
2025-01-24 14:46:45 +0900

    Title: 1시간 뒤 시간 출력, Time: 119ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 250124/1시간 뒤 시간 출력/print-one-hour-later.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+
+        sc.useDelimiter(":");
+
+        int hour = sc.nextInt();
+        int min = sc.nextInt();
+
+        System.out.printf("%d:%d",hour + 1, min);
+    }
+}
