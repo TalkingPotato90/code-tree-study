fcbc67cceb658d04b41dca20222641c2bd682597
TalkingPotato90 <138276030+TalkingPotato90@users.noreply.github.com>
2025-01-24 11:41:06 +0900

    Title: 소수점 반올림하기, Time: 93ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 250124/소수점 반올림하기/rounding-decimal-points.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        float a = 25.352f;
+
+        System.out.printf("%.1f",a);
+    }
+}
