90df641d7ae49e2230836a8eed8e714b03b13277
TalkingPotato90 <138276030+TalkingPotato90@users.noreply.github.com>
2025-01-29 17:51:27 +0900

    Title: 4가지 관계연산자, Time: 130ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 250129/4가지 관계연산자/four-relational-operators.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        check(a >= b);
+        check(a > b);
+        check(b >= a);
+        check(b > a);
+    }
+
+    private static void check(boolean b){
+        int a = b ? 1 : 0;
+        System.out.println(a);
+    }
+}
