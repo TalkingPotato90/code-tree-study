5ba790c0b6a9522fcc038ee0e089c640fe4ee8ed
TalkingPotato90 <138276030+TalkingPotato90@users.noreply.github.com>
2025-02-18 15:32:26 +0900

    Title: 중복되지 않는 정수 중 최대, Time: 201ms, Memory: 13MB, Status: Wrong Answer - CodeTree

---
+++ 250218/중복되지 않는 정수 중 최대/max-of-unique-number.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int max = Integer.MIN_VALUE;
+        int[] arr = new int[n];
+
+        for(int i = 0; i < n; i++){
+            arr[i] = sc.nextInt();
+        }
+
+        for(int i = 0; i < n; i++){
+            for(int j = i + 1; j < n; j++){
+                if(arr[i] == arr[j]){
+                    arr[i] = -1;
+                    arr[j] = -1;
+                }
+            }
+        }
+
+        for(int i = 0; i < n; i++){
+            if(arr[i] > max){
+                max = arr[i];
+            }
+        }
+
+        System.out.println(max);
+    }
+}
